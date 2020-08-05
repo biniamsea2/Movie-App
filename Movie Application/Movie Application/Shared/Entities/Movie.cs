0a8a1cbb647fdb9216876d18a69bcd2ac78a23d9
Biniam <Biniamt23@yahoo.com>
2020-08-04 23:38:46 -0700

    conditionals set, loading text and movies added

---
+++ Movie Application/Movie Application/Shared/Entities/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Movie_Application.Shared.Entities
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+    }
+}

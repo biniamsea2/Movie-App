bd52ed2936345de188270c70518179a203e5f363
Biniam <Biniamt23@yahoo.com>
2020-08-14 00:38:02 -0700

    invoking static c# methods from javascipt

--- Movie Application/Movie Application/Client/Helper/IJSRuntimeExtensionMethods.cs
+++ Movie Application/Movie Application/Client/Helper/IJSRuntimeExtensionMethods.cs
@@ -10,7 +10,13 @@ namespace Movie_Application.Client.Helper
     {
         public static async ValueTask<bool> Confirm(this IJSRuntime js, string message)
         {
+            await js.InvokeVoidAsync("Console.log", "example message");
             return await js.InvokeAsync<bool>("confirm", message);
+        }
+
+        public static async ValueTask MyFunction(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("my_function", message);
         }
     }
 }
--- Movie Application/Movie Application/Client/wwwroot/js/Utilites.js
+++ Movie Application/Movie Application/Client/wwwroot/js/Utilites.js
@@ -1,3 +1,10 @@
 ﻿function my_function(message) {
     console.log("from utilies" + message);
 }
+
+function dotnetStaticInvocation() {
+    DotNet.invokeMethodAsync("Movie_Application.Client", "GetCurrentCount")
+        .then(result => {
+            console.log("count from javascript" + result);
+        });
+}

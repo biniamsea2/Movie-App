247de2ee0bfd4d311c4a25cba20edc0264ffa39a
Biniam <Biniamt23@yahoo.com>
2020-08-19 00:38:36 -0700

    poster added and cleaned up code

--- Movie Application/Movie Application/Client/Pages/Counter.razor.cs
+++ Movie Application/Movie Application/Client/Pages/Counter.razor.cs
@@ -11,49 +11,12 @@ namespace Movie_Application.Client.Pages
 {
     public partial class Counter
     {
-        //use the attribute inject and make it a property in order to use it here
-        [Inject] SingletonService singleton { get; set; }
-        [Inject] TransientService transient { get; set; }
-        [Inject] IJSRuntime js { get; set; }
-        [CascadingParameter] public AppState AppState { get; set; }
-
         private int currentCount = 0;
-        private static int currentCountStatic = 0;
-
-        private List<Movie> movies;
-        // OnInitialized runs when our razor file loads
-        protected override void OnInitialized()
-        {
-            //await Task.Delay(3500);
-            movies = new List<Movie>()
-            {
-            new Movie () {Title = "Logan", ReleaseDate = new DateTime(2017, 3, 3)},
-            new Movie () {Title = "Kong: Skull Island", ReleaseDate = new DateTime(2017, 3, 10)},
-            new Movie () {Title = "Jumanji: Welcome to the Jungle", ReleaseDate = new DateTime(2017, 12, 20)},
-            };
-        }
-
-        [JSInvokable]
-        public async Task IncrementCount()
+        public void IncrementCount()
         {
             currentCount++;
-            transient.Value = currentCount;
-            singleton.Value = currentCount;
-            currentCountStatic++;
-            await js.InvokeVoidAsync("dotnetStaticInvocation");
         }
 
-        [JSInvokable]
-        public static Task<int> GetCurrentCount()
-        {
-            return Task.FromResult(currentCountStatic);
-        }
-
-        private async Task IncrementCountJavascipt()
-        {
-            await js.InvokeVoidAsync("dotnetInstanceInvocation",
-                DotNetObjectReference.Create(this));
-        }
 
 
     }
--- Movie Application/Movie Application/Shared/Entities/Movie.cs
+++ Movie Application/Movie Application/Shared/Entities/Movie.cs
@@ -10,5 +10,24 @@ namespace Movie_Application.Shared.Entities
         public string Title { get; set; }
         [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+        public string Poster { get; set; }
+        public string TitleBrief
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(Title))
+                {
+                    return null;
+                }
+                if (Title.Length > 60)
+                {
+                    return Title.Substring(0, 60) + "...";
+                }
+                else
+                {
+                    return Title;
+                }
+            }
+        }
     }
 }

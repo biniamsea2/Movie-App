6029ed43bb0489860c7de519446b413aa554ec13
Biniam <Biniamt23@yahoo.com>
2020-08-21 01:01:44 -0700

    forms for cast members added, with edit feature

---
+++ Movie Application/Movie Application/Shared/Entities/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Movie_Application.Shared.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Biography { get; set; }
+        public string Picture { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}

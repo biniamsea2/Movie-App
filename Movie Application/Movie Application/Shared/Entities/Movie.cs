92fc9bac783370531f76725b2bc78b20d13f082c
Biniam <Biniamt23@yahoo.com>
2020-08-24 23:47:39 -0700

    Multiple selector component created

--- Movie Application/Movie Application/Shared/Entities/Movie.cs
+++ Movie Application/Movie Application/Shared/Entities/Movie.cs
@@ -17,6 +17,7 @@ namespace Movie_Application.Shared.Entities
         [Display(Name = "Release Date")]
         public DateTime? ReleaseDate { get; set; }
         public string Poster { get; set; }
+        public List<MoviesGenres> MoviesGenres { get; set; } = new List<MoviesGenres>();
         public string TitleBrief
         {
             get

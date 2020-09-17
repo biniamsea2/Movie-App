51a577c7dc7ff7cce408264f276ae80da50be278
Biniam <Biniamt23@yahoo.com>
2020-09-16 23:48:10 -0700

    error fixed, needed to target the server in the package manager console dropdown box

--- Movie Application/Movie Application/Server/Startup.cs
+++ Movie Application/Movie Application/Server/Startup.cs
@@ -19,7 +19,6 @@ namespace Movie_Application.Server
            this.configuration = configuration;
         }
 
-        //public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940

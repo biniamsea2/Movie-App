48f9dfe167ce5ae1e9d423d6fe31eea3e37cb56a
Biniam <Biniamt23@yahoo.com>
2020-09-17 20:48:07 -0700

    gettin error on create genre method

---
+++ Movie Application/Movie Application/Server/Controllers/GenreController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movie_Application.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_Application.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GenreController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public GenreController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Genre genre)
+        {
+            context.Add(genre);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}

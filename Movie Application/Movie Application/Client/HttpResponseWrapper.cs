7a3fe8736749221389ad8ba42a2b2e34ee273d7a
Biniam <Biniamt23@yahoo.com>
2020-09-17 21:08:39 -0700

    peopleController created

--- Movie Application/Movie Application/Client/HttpResponseWrapper.cs
+++ Movie Application/Movie Application/Client/HttpResponseWrapper.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace Movie Application.Client
+namespace Movie_Application.Client
 {
-    public class HrrpResponseWrapper
-{
-}
+    public class HttpResponseWrapper<T>
+    {
+        public HttpResponseWrapper(T response, bool success, HttpResponseMessage httpResponseMessage)
+        {
+            Success = success;
+            Response = response;
+            HttpResponseMessage = HttpResponseMessage;
+        }
+        public bool Success { get; set; }
+        public T Response { get; set; }
+        public HttpResponseMessage HttpResponseMessage { get; set; }
+        public async Task<string> GetBody()
+        {
+            return await HttpResponseMessage.Content.ReadAsStringAsync();        }
+    }
 }

a29e179e18bc1f7476e96f72ac81d7ef43c106be
Biniam <Biniamt23@yahoo.com>
2020-09-17 21:09:08 -0700

    peopleController created

--- Movie Application/Movie Application/Server/Controllers/PeopleController.cs
+++ Movie Application/Movie Application/Server/Controllers/PeopleController.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Movie Application.Server.Controllers
+namespace Movie_Application.Server.Controllers
 {
     public class PeopleController
-{
-}
+    {
+    }
 }

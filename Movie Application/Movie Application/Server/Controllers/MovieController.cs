c2fcc8c4f87a73f535ed9b7aeb819a4271b7e638
Biniam <Biniamt23@yahoo.com>
2020-09-18 18:27:03 -0700

    movie controller added

---
+++ Movie Application/Movie Application/Server/Controllers/MovieController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movie_Application.Server.Helpers;
+using Movie_Application.Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_Application.Server.Controllers
+{
+    public class MovieController
+    {
+        [ApiController]
+        [Route("api/[controller]")]
+        public class MovieController : ControllerBase
+        {
+            private readonly ApplicationDbContext context;
+            private readonly IFileStorageService fileStorageService;
+            public MovieController(ApplicationDbContext context, IFileStorageService fileStorageService)
+            {
+                this.context = context;
+                this.fileStorageService = fileStorageService;
+            }
+
+            [HttpPost]
+            public async Task<ActionResult<int>> Post(Movie movie)
+            {
+                if (!string.IsNullOrWhiteSpace(movie.Poster))
+                {
+                    var poster = Convert.FromBase64String(movie.Poster);
+                    movie.Poster = await fileStorageService.SaveFile(poster, "jpg", "movies");
+                }
+                context.Add(movie);
+                await context.SaveChangesAsync();
+                return movie.Id;
+            }
+        }
+    }
+}

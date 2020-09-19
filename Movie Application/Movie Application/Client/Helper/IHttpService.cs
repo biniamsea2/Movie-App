40f074e8782d65d035b26a9be3741eb0be8c34a3
Biniam <Biniamt23@yahoo.com>
2020-09-18 19:24:12 -0700

    movieController finished, fixed imports issue

--- Movie Application/Movie Application/Client/Helper/IHttpService.cs
+++ Movie Application/Movie Application/Client/Helper/IHttpService.cs
@@ -8,5 +8,6 @@ namespace Movie_Application.Client.Helper
     public interface IHttpService
     {
         Task<HttpResponseWrapper<object>> Post<T>(string url, T data);
+        Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T data);
     }
 }
